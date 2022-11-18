e381e09da186c167547d4a849f0cee869689750c
smithm4949 <smithm4949@gmail.com>
2022-11-18 08:43:04 -0500

    First try at password generator: wrote generatePassword function

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Initialize arrays of options one time
+let specialChars = [' ', '!', '"', '#', '$', '%', '&',
+"'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';',
+'<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
+let nums = ['1','2','3','4','5','6','7','8','9','0'];
+let capitals = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,6 +20,45 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+// return string of generated password
+function generatePassword() {
+  let desiredLength = prompt('How long do you want your password to be?');
+  if (parseInt(length) == NaN || length < 8 || length > 120) {
+    alert('You must enter a number between 8 and 120 for the length');
+    return;
+  }
+
+  let characterOptions = [];
+  let password = '';
+
+  if (confirm('Click OK to include special characters in your password:')) {
+    characterOptions.concat(specialChars);
+  }
+
+  if (confirm('Click OK to include numbers in your password:')) {
+    characterOptions.concat(nums);
+  }
+
+  if (confirm('Click OK to include capital letters in your password:')) {
+    characterOptions.concat(specialChars);
+  }
+
+  if (confirm('Click OK to include lowercase letters in your password:')) {
+    characterOptions.concat(specialChars);
+  }
+
+  if (characterOptions.length == 0) {
+    alert("Error, cannot create password without at least one set of options. Try again.");
+    return;
+  }
+
+  for (let i = 0; i < desiredLength; i++) {
+    password += characterOptions[Math.floor(Math.random() * characterOptions.length)];
+  }
+
+  return password;
 }
 
 // Add event listener to generate button
